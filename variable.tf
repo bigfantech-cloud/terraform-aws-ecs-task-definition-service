876e1c1ae47a9e4e16e30055b65a00088840c0f3
bigfantech <130840188+bigfantech@users.noreply.github.com>
2023-04-17 15:50:44 +0530

    Change: ignore_task_definition_change value

--- variable.tf
+++ variable.tf
@@ -64,7 +64,7 @@ variable "ecs_task_desired_count" {
 variable "ignore_task_definition_change" {
   description = "Whether to ignore updating ECS Service when new Task Definition is created through Terraform"
   type        = bool
-  default     = ture
+  default     = true
 }
 
 variable "lb_target_group_arn" {
